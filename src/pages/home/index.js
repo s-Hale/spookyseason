f869a5d9989e84c6e0c88a48ec97abd3ff9d0bc4
Sally Hale <sally.hale@brighthr.com>
2021-10-04 09:59:26 +0100

    add icons to project

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import UserInput from './components/UserInput/index'
+import { ReactComponent as Cauldron } from '../../svg/cauldron.svg'
+import { ReactComponent as Mortar } from '../../svg/mortar.svg'
+import { ReactComponent as Ball } from '../../svg/006-magic-ball.svg'
+import { ReactComponent as Herb } from '../../svg/003-leaves.svg'
+import { ReactComponent as Moon } from '../../svg/007-moon.svg'
+
+
+
+
+
+
+
 
 const Home = () => {
     return (
-        <div className="flex flex-col h-60 border border-red-500 justify-center items-center">spooky season</div>
+        <>
+            <div className="font-josefin-sans flex flex-col h-60 justify-center items-center">Craft me a rite for...<UserInput /></div>
+            <div className="flex w-2/3 justify-between">
+                <Cauldron />
+                <Mortar/>
+                <Ball />
+                <Herb />
+                <Moon/>
+            </div>
+            </>
     )
 };
 
