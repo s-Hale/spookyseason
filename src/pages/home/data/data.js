05c16bf872dd52e1db68f390b6c6803a2bcecbe7
Sally Hale <sally.hale@brighthr.com>
2021-10-05 18:02:17 +0100

    results implemented

---
+++ src/pages/home/data/data.js
@@ -0,0 +1,26 @@
+
+export const herbs = [
+    {
+        name: 'betony', properties: ['courage', 'love', 'health']
+    },
+    {
+        name: 'mugwort', properties: ['health']
+    },
+    {
+        name: 'sage', properties: ['courage', 'strength', 'health']
+    }
+];
+
+
+export const crystals = [
+    {name:'amethyst', properties:['grounding', 'salt', 'love']},
+    {name:'garnet', properties:['love', 'healing']},
+    {name:'agate', properties:['courage', 'health', 'children', 'love']},
+    {name:'carnelian', properties:['courage', 'love', 'darkness']},
+];
+
+export const phases = [
+    {name:'waxing', properties:['courage', 'growth', 'strength', 'confidence']},
+    {name:'waning', properties:['banish', 'release', 'health']}
+  ];
+
